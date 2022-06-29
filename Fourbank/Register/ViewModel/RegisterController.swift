e73859babfa9bd4838575ce05e55fb4a5222c38b
Daniel <daniel.oliveira@foursys.com.br>
2022-06-29 12:38:40 -0300

    Correção de bugs em pastas que apagavam arquivo ao subir no gitlab

--- Fourbank/Register/ViewModel/RegisterController.swift
+++ Fourbank/Register/ViewModel/RegisterController.swift
@@ -10,40 +10,6 @@ import CoreData
 import Alamofire
 
 class RegisterController: UIViewController {
-     
-    func registerAPI(_ name: String,
-                     _ birthDate: String,
-                     _ cpf: String,
-                     _ cellPhone: String,
-                     _ email: String,
-                     _ password: String,
-                     _ street: String,
-                     _ cep: String,
-                     _ district: String,
-                     _ city: String,
-                     _ state: String) {
-
-        let parameters: [String: Any] = ["name": name as Any,
-                                         "birthDate": birthDate as Any,
-                                         "cpf": cpf as Any,
-                                         "cellPhone": cellPhone as Any,
-                                         "email": email as Any,
-                                         "password": password as Any,
-                                         "street": street as Any,
-                                         "cep": cep as Any,
-                                         "district": district as Any,
-                                         "city": city as Any,
-                                         "state": state as Any]
-        
-
-        AF.request("https://62baed237bdbe01d52938975.mockapi.io/api/users",
-                   method: .post,
-                   parameters: parameters,
-                   encoding: JSONEncoding.default).responseJSON {response in
-
-            print("Success")
-        }
-    }
     
     func registerCoreData(_ name: String?,
                           _ cpf: String?,
